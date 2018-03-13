b3b5029484149729c97bbc93e73c8470bbdf50ca
michaelgeraghty <geraghtymichael95@gmail.com>
2018-03-13 20:33:34 +0000

    Added NUnit test class blank

---
+++ NunitTest/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Business;
+
+namespace NunitTest
+{
+    [TestFixture]
+    class Test
+    {
+        Bll businessTest = new Bll();
+
+        [Test]
+        public void TestShowTable()
+        {
+            
+        }
+        
+    }
+}
